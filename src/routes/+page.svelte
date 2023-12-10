8bf625bd7a852be4b6675e81bca834bc1e131f62
Colin Bagley <cjbagley@googlemail.com>
2023-12-10 16:36:00 +0000

    Initial commit: Svelte skeleton app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="app">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+	.app {
+		height: 100vh;
+		background-size: 40px 40px;
+		background-image: linear-gradient(to right, grey 1px, transparent 1px),
+			linear-gradient(to bottom, grey 1px, transparent 1px);
+	}
+</style>
