2ed4edc94a06f5cb1a65ff474433a7ad716a2646
Colin Bagley <cjbagley@googlemail.com>
2023-12-14 12:38:49 +0000

    [WIP] Tweaking layout/game page

--- src/routes/browse/games/+page.svelte
+++ src/routes/browse/games/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Content from '../../../components/content.svelte';
-	import GameImage from '../../../components/game-image.svelte';
+	import GameLink from '../../../components/game-link.svelte';
 </script>
 
 <svelte:head>
@@ -8,13 +8,8 @@
 </svelte:head>
 
 <Content header="Browse Games">
-	<div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-		<div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-			<GameImage />
-			<GameImage />
-			<GameImage />
-			<GameImage />
-			<GameImage />
-		</div>
+	<div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+		<GameLink />
+		<GameLink />
 	</div>
 </Content>
