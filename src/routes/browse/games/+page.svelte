de6335a306246795649b750323c6e6d4ec0cd550
Colin Bagley <cjbagley@googlemail.com>
2023-12-14 11:58:43 +0000

    [WIP] Working on games page layout POC

--- src/routes/browse/games/+page.svelte
+++ src/routes/browse/games/+page.svelte
@@ -1,9 +1,20 @@
 <script>
 	import Content from '../../../components/content.svelte';
+	import GameImage from '../../../components/game-image.svelte';
 </script>
 
 <svelte:head>
 	<title>Browse Games</title>
 </svelte:head>
 
-<Content header="Browse Games">Games page</Content>
+<Content header="Browse Games">
+	<div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+		<div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+			<GameImage />
+			<GameImage />
+			<GameImage />
+			<GameImage />
+			<GameImage />
+		</div>
+	</div>
+</Content>
