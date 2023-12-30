35729f3bce221e4cd7602fe6ae5d14c1a1194ca6
Colin Bagley <cjbagley@googlemail.com>
2023-12-30 12:30:23 +0000

    [WIP] Adding Laravel Breeze

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
 module.exports = {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.svelte",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [forms],
     darkMode: 'class',
 };
