7b9f8f03196c957028ea0541e9a2e75836873aea
Colin Bagley <cjbagley@googlemail.com>
2024-01-01 13:19:19 +0000

    [WIP] Adding game models, migration etc.

---
+++ app/Http/Controllers/Admin/GameController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Requests\StoreGameRequest;
+use App\Http\Requests\UpdateGameRequest;
+use App\Models\Game;
+
+class GameController extends AuthController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreGameRequest $request)
+    {
+        //
+    }
+
+    public function show(Game $game)
+    {
+        //
+    }
+
+    public function edit(Game $game)
+    {
+        //
+    }
+
+    public function update(UpdateGameRequest $request, Game $game)
+    {
+        //
+    }
+
+    public function destroy(Game $game)
+    {
+        //
+    }
+}
