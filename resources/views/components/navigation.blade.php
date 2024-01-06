0f5792560d8416436f38f7bc3a499cc8a387cf78
Colin Bagley <cjbagley@googlemail.com>
2024-01-06 13:52:53 +0000

    [WIP] Responsive admin dropdown links

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -68,12 +68,6 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
@@ -82,8 +76,16 @@
             </div>
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
                     {{ __('Profile') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('lookup.form')" :active="request()->routeIs('lookup.form')">
+                    {{ __('IGDB Lookup') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
