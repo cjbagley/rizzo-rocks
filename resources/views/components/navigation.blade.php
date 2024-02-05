ce8e5d07f689a30c19dd6351cbf23f62c41487dc
Colin Bagley <cjbagley@googlemail.com>
2024-02-05 14:12:15 +0000

    Converting admin from tailwind to css

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,106 +1,31 @@
 <nav x-data="{ open: false }">
-    <div class="nav-wrapper">
+    <div class="admin-nav">
         <div class="nav-container">
-            <div class="nav-wrapper">
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                    {{ __('Dashboard') }}
-                </x-nav-link>
-            </div>
-            <div class="nav-wrapper">
-                <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
-                    {{ __('Profile') }}
-                </x-nav-link>
-            </div>
-            <div class="nav-wrapper">
-                <x-nav-link :href="route('lookup.form')" :active="request()->routeIs('lookup.form')">
-                    {{ __('IGDB Lookup') }}
-                </x-nav-link>
-            </div>
-            <div class="nav-wrapper">
-                <x-nav-link :href="route('games.index')" :active="request()->routeIs('games.index')">
-                    {{ __('Games') }}
-                </x-nav-link>
-            </div>
+            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
+            </x-nav-link>
+            <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
+                {{ __('Profile') }}
+            </x-nav-link>
+            <x-nav-link :href="route('lookup.form')" :active="request()->routeIs('lookup.form')">
+                {{ __('IGDB Lookup') }}
+            </x-nav-link>
+            <x-nav-link :href="route('games.index')" :active="request()->routeIs('games.index')">
+                {{ __('Games') }}
+            </x-nav-link>
         </div>
 
-        <!-- Settings Dropdown -->
-        <div class="settings-container">
-            <x-dropdown align="right" width="48">
-                <x-slot name="trigger">
-                    <button>
-                        <div>{{ Auth::user()->name }}</div>
+        <div class="profile-container">
+            <div>{{ Auth::user()->name }}</div>
 
-                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
-                    </button>
-                </x-slot>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
 
-                <x-slot name="content">
-                    <x-dropdown-link :href="route('profile.edit')">
-                        {{ __('Profile') }}
-                    </x-dropdown-link>
-
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-
-                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                <a href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                            {{ __('Log Out') }}
-                        </x-dropdown-link>
-                    </form>
-                </x-slot>
-            </x-dropdown>
-        </div>
-
-        <!-- Hamburger -->
-        <div class="-me-2 flex items-center sm:hidden">
-            <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-        </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                    {{ __('Dashboard') }}
-                </x-responsive-nav-link>
-
-                <x-responsive-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <x-responsive-nav-link :href="route('lookup.form')" :active="request()->routeIs('lookup.form')">
-                    {{ __('IGDB Lookup') }}
-                </x-responsive-nav-link>
-
-                <x-responsive-nav-link :href="route('games.index')" :active="request()->routeIs('games.index')">
-                    {{ __('Games') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
+                    {{ __('Log Out') }}
+                </a>
+            </form>
         </div>
     </div>
 </nav>
