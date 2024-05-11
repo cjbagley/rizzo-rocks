f3f3d3672f1c3a538f2b807014b80ad58f1cf054
Colin Bagley <cjbagley@googlemail.com>
2024-05-11 12:52:45 +0100

    feat: Added Game capture display

--- resources/js/Components/Video.svelte
+++ resources/js/Components/Video.svelte
@@ -1,12 +1,41 @@
 <script>
     import Content from "../Components/Content.svelte";
 
-    export let videos;
+    export let video;
 </script>
 
 <Content>
-    <video controls>
-        Your browser does not support video :(
-        <source src="#" type="video/webm"/>
-    </video>
+    <div class="wrapper">
+        <video controls controlslist="nodownload" disablepictureinpicture="true" playsinline="false">
+            Your browser does not support video :(
+            <source src="{video.href}" type="video/webm"/>
+        </video>
+        <div>
+            <h2>{video.title}</h2>
+            <p>{video.comments}</p>
+        </div>
+    </div>
 </Content>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: column-reverse;
+    }
+
+    video {
+        margin: 0 auto;
+        max-width: 500px;
+    }
+
+    @media (width >= 1024px) {
+        .wrapper {
+            flex-direction: row;
+            column-gap: 20px;
+        }
+    }
+
+    p {
+        margin: 10px 0;
+    }
+</style>
